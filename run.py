155ab75b5d85550a677ce5869ca5a4c11d353aaf
TimBud <tv-budygin@yandex.ru>
2025-04-10 00:33:19 +0300

    сделал правильное распределение

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template, request, redirect, abort, flash
+from flask import Flask, render_template, request, redirect, abort
 from connect_api.Captcha_api import check_captcha
 from connect_api.YandexGPT_api import gpt
-from sqlalchemy import or_
 from data.user import User
 from data import db_session
 from forms.registration import RegisterForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from forms.login import LoginForm
 from data.food import Food
+from data.request_to_db import DataBase
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'timbudygin./././'
+db = DataBase(User, Food, db_session)
 
 
 @login_manager.user_loader
@@ -89,46 +90,14 @@ def main_menu():
     params = {"option1": "1 Вариант Рецепта", "option2": "2 Вариант Рецепта",
               "option3": "3 Вариант Рецепта", "text": "center", "pad": "100px"}
     if request.method == "POST":
-        db_sess = db_session.create_session()
         if "render" in request.form:
             product = request.form.get("input1")
             wish = request.form.get("input2")
-            text = gpt(product, wish).split("\n")
-            params = {"text": "left", "pad": "20px"}
-            text1 = []
-            res_his = []
-            k = 1
-            for i in text:
-                if i != "":
-                    text1.append(i)
-                else:
-                    if k <= 3:
-                        params["option" + str(k)] = "<br>".join(text1)
-                        res_his.append(text1[0][:-1])
-                        k += 1
-                    text1 = []
-            params["option" + str(k)] = "<br>".join(text1)
-            res_his.append(text1[0][:-1])
-            food = Food(
-                history=f"{product}; {wish}" if product != "" and wish != "" else f"{product}{wish}",
-                user_id=current_user.id,
-                result_his=", ".join(res_his)
-            )
-            db_sess.add(food)
-            db_sess.commit()
+            params = db.db_main_btn_render(current_user, product, wish, gpt)
         if "like" in request.form:
-            like_text = request.form.get("like")
-            if ("1 Вариант Рецепта" != like_text and "2 Вариант Рецепта" != like_text
-                    and "3 Вариант Рецепта" != like_text):
-                params = {"text": "left", "pad": "20px", "option1": request.form.get("option1"),
-                          "option2": request.form.get("option2"), "option3": request.form.get("option3")}
-                food = Food(
-                    like_title=like_text[like_text.find(".") + 2:like_text.find("<br>") - 1],
-                    like=like_text[like_text.find("<br>") + 4:],
-                    user_id=current_user.id
-                )
-                db_sess.add(food)
-                db_sess.commit()
+            try_params = db.db_main_btn_like(request, current_user)
+            if try_params:
+                params = try_params
     return render_template('index.html', **params)
 
 
@@ -136,21 +105,13 @@ def main_menu():
 def profile():
     if not current_user.is_authenticated:
         return redirect("/login")
-    db_sess = db_session.create_session()
-    user = db_sess.query(User).filter(User.id == current_user.id).first()
-    params = {"name": user.name, "email": user.email, "create_data": user.created_date}
+    params = db.db_profile(current_user)
     return render_template("profile.html", **params)
 
 
 @app.route("/delete", methods=["POST"])
 def delete():
-    db_sess = db_session.create_session()
-    food = db_sess.query(Food).filter(Food.user_id == current_user.id).all()
-    user = db_sess.query(User).filter(User.id == current_user.id).first()
-    db_sess.delete(user)
-    for i in food:
-        db_sess.delete(i)
-    db_sess.commit()
+    db.db_delete_yourself(current_user)
     logout_user()
     return redirect("/")
 
@@ -158,14 +119,7 @@ def delete():
 @app.route("/delete/<string:user_email>/<int:num_of_req>", methods=["POST"])
 def delete_user(user_email, num_of_req):
     if num_of_req > 10:
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == user_email).first()
-        if user:
-            food = db_sess.query(Food).filter(Food.user_id == user.id)
-            for i in food:
-                db_sess.delete(i)
-        db_sess.delete(user)
-        db_sess.commit()
+        db.db_delete_user(user_email)
         return redirect("/history")
     else:
         abort(418, "У пользователя должно быть хотя бы 10 запросов")
@@ -175,44 +129,15 @@ def delete_user(user_email, num_of_req):
 def likes():
     if not current_user.is_authenticated:
         return redirect("/login")
-    if current_user:
-        like = []
-        db_sess = db_session.create_session()
-        food = db_sess.query(Food).filter(Food.user_id == current_user.id,
-                                          or_(Food.like_title.isnot(None), Food.like.isnot(None))).all()
-        for i in food:
-            like.append([i.like_title, i.like])
-        return render_template("likes.html", quant=len(like), likes=like)
+    like = db.db_likes(current_user)
+    return render_template("likes.html", quant=len(like), likes=like)
 
 
 @app.route("/history", methods=["GET", "POST"])
 def history():
     if not current_user.is_authenticated:
         return redirect("/login")
-    hist = []
-    db_sess = db_session.create_session()
-    admins = db_sess.query(User).filter(User.id == current_user.id).first()
-    food = db_sess.query(Food).filter(Food.user_id == current_user.id, Food.result_his.isnot(None)).all()
-    for i in food:
-        hist.append([i.history, i.result_his])
-    params = {"quant": len(hist), "hist": hist}
-    if admins.admin:
-        user_hist = []
-        user_food = db_sess.query(Food).filter(Food.user_id == User.id,
-                                               Food.like == None, Food.like_title == None).all()
-        for i in user_food:
-            name_user = db_sess.query(User).filter(User.id == i.user_id).first()
-            done_user = False
-            for e in user_hist:
-                if name_user.email in e:
-                    done_user = True
-                    e[1] += 1
-                    break
-            if not done_user and name_user.email != current_user.email:
-                user_hist.append([name_user.email, 1])
-        params["user_quant"] = len(user_hist)
-        params["user_hist"] = user_hist
-        params["admin"] = True
+    params = db.db_history(current_user)
     return render_template("history.html", **params)
 
 
